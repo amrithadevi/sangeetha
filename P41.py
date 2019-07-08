1991bf629d7ec827fecf6ed3b16e9f12e1eb4763
Sangeetha.M <sangeethameganathan98@gmail.com>
2019-07-08 15:58:07 +0530

    Update P41.py

--- P41.py
+++ P41.py
@@ -1,19 +1,19 @@
-ain,b=input().split()
+ain,bb=input().split()
 ain=int(ain)
-b=int(b)
+bb=int(bb)
 s=''
 u=2
-if(ain+b<=3):
-    for i in range(0,ain+b):
+if(ain+bb<=3):
+    for i in range(0,ain+bb):
         if(i%2!=0):
             s=s+'0'
         else:
             s=s+'1'
 else:    
-    for i in range(0,ain+b):
+    for i in range(0,ain+bb):
         if(i==u):
             s=s+'0'
-            if(u==b):
+            if(u==bb):
                 u=u+2
             else:
                 u=u+3
@@ -22,7 +22,7 @@ else:
 x=len(s)-1
 if(int(s[x])==0):
     print('-1')
-elif ain==1 and b==2: 
+elif ain==1 and bb==2: 
      print("011")
 else:
     print(s)
